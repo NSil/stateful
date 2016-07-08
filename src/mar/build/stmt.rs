2ffd4398398fd1f89567e804364d323c8e945f9b
NSil <n0987b@gmail.com>
2016-07-08 03:19:46 +0300

    Clean up the code and explain a bit

--- src/mar/build/stmt.rs
+++ src/mar/build/stmt.rs
@@ -55,24 +55,36 @@ impl<'a, 'b: 'a> Builder<'a, 'b> {
             self.cx.span_bug(span, &format!("Local variables need initializers at the moment"));
         }
 
-        let block2 = self.expr(extent, block, &local.init.clone().unwrap());
+        // TODO: maybe stop using `expr()` here - it mutates self and makes everything a lot more fragile.
+        let end_scope_block = self.expr(extent, block, &local.init.clone().unwrap());
 
-        let init_stmt = if block == block2 {
+        // `expr()` returns a different block than the one provided it only if the block contained
+        // a state transition. This is handled differently.
+        let init_stmt = if block == end_scope_block {
+
+            // `expr()` simply adds its expression as a statement to the block if there was no state change.
             self.cfg.basic_blocks[block.index()].statements.pop().unwrap()
         } else {
-            let block_index = block2.index() as usize - 1;
-            {
-                let decls = self.cfg.basic_blocks[block_index].decls().to_owned();
+            // This branch means that the `let` binding had a state change in its initialization expression.
 
-                let let_block = &mut self.cfg.basic_blocks[block2.index()];
+            let last_statement_block_index = end_scope_block.index() as usize - 1;
+
+            {
+                let decls = self.cfg.basic_blocks[last_statement_block_index].decls().to_owned();
+
+                // The initialization expression might need variables from the previous block, so we copy the decls, removing duplicates.
+                let let_block = &mut self.cfg.basic_blocks[end_scope_block.index()];
                 for (decl, ident) in decls {
                     if !let_block.decls.iter().any(|&(_, x)| x.name == ident.name) {
                         let_block.decls.push((decl, ident));
                     }
                 }
             }
-            let init_index = self.cfg.basic_blocks[block_index].statements.iter().enumerate()
+
+            // Take the final expression from the block and use it to as the initializtion expression for the binding.
+            let init_index = self.cfg.basic_blocks[last_statement_block_index].statements.iter().enumerate()
                 .filter(|&(_, block_statement)| {
+                    println!("stmt: {:#?}", block_statement);
                     match block_statement {
                         &Statement::Expr(..) => true,
                         _ => false,
@@ -80,9 +92,11 @@ impl<'a, 'b: 'a> Builder<'a, 'b> {
                 })
                 .map(|(idx, _)| idx).next().unwrap();
 
-            self.cfg.basic_blocks[block_index].statements.remove(init_index)
+            self.cfg.basic_blocks[last_statement_block_index].statements.remove(init_index)
         };
 
+        // It lonly makes sense for an expression to be here.
+        // TODO: handle other statements, since they are slso technically expressions.
         let init_stmt = Some(match init_stmt {
             Statement::Expr(ref stmt) => {
                 match stmt.node {
@@ -95,19 +109,18 @@ impl<'a, 'b: 'a> Builder<'a, 'b> {
             }
         }.clone());
 
-        let block = block2;
 
         for (decl, _) in self.get_decls_from_pat(&local.pat) {
             let lvalue = self.cfg.var_decl_data(decl).ident;
 
             let alias = self.find_decl(lvalue).map(|alias| {
-                self.alias(block, span, alias)
+                self.alias(end_scope_block, span, alias)
             });
 
             self.schedule_drop(span, extent, decl, alias);
         }
 
-        self.cfg.push(block, Statement::Let {
+        self.cfg.push(end_scope_block, Statement::Let {
             span: span,
             pat: local.pat.clone(),
             ty: local.ty.clone(),
@@ -115,7 +128,7 @@ impl<'a, 'b: 'a> Builder<'a, 'b> {
             init: init_stmt,
         });
 
-        block
+        end_scope_block
     }
 
     fn alias(&mut self,
